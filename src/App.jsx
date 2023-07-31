98cdbddfa1fc4ee1d58ab5eadb44fe7af5ca03ce
André de Souza <andre_aru010@hotmail.com>
2023-07-31 13:45:46 -0300

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./styles/App.css";
 
 function App() {
 
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
