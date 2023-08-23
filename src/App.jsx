df495749e30afcf86fef52fcc29d109571b4ed30
André de Souza <andre_aru010@hotmail.com>
2023-08-23 17:03:44 -0300

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import { useState } from "react";
-import Header from "./components/header/Header"
+import Header from "./components/header/Header";
 import { Main } from "./components/main/Main";
 import "./styles/App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-
   const [isDarkMode, setIsDarkMode] = useState(true);
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
 
-    console.log(isDarkMode)
+    console.log(isDarkMode);
   };
 
   return (
-
-    <div className={isDarkMode ? 'dark' : 'light-mode'}>
+    <div className={isDarkMode ? "dark" : "light-mode"}>
       <Header toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
       <Main />
+      <Analytics />
     </div>
-
-  )
+  );
 }
 
 export default App;
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,17 +1,17 @@
-import { Footer } from '../footer/Footer';
-import { About } from './about/About';
-import Contact from './contact/Contact';
-import { Hero } from './hero/Hero';
-import { Projects } from './projects/Projects';
+import { Footer } from "../footer/Footer";
+import { About } from "./about/About";
+import Contact from "./contact/Contact";
+import { Hero } from "./hero/Hero";
+import { Projects } from "./projects/Projects";
 
 export const Main = () => {
   return (
-    <main className='main'>
+    <main className="main">
       <Hero />
       <About />
       <Projects />
       <Contact />
       <Footer />
     </main>
-  )
-}
+  );
+};
